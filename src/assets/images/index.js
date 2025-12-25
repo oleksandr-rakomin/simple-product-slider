b9cab177b5a641a28ddfd0ec9e0e84cb650c1d22
iq.dev.expert <forprivateuse3000@gmail.com>
2025-12-25 09:24:29 +0100

    init project

---
+++ src/assets/images/index.js
@@ -0,0 +1,4 @@
+export { default as angryPikachuPokemon } from "./angry-pikachu-pokemon.png";
+export { default as bulbasaurPokemon } from "./bulbasaur-pokemon.png";
+export { default as meowthPokemon } from "./meowth-pokemon.png";
+export { default as nidoqueenPokemon } from "./nidoqueen-pokemon.png";
